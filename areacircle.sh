7bb1ab0ad6e7a060357ef963e352ded4c0a31c5c
Amal070 <154653845+Amal070@users.noreply.github.com>
2025-06-05 11:47:09 +0530

    Exp:4.3- area & circumference of circle

--- areacircle.sh
+++ areacircle.sh
@@ -6,7 +6,7 @@ circum=$(echo "2 * 3.14 * $r" | bc)
 echo "Area of the circle: $area"
 echo "Circumference of the circle: $circum"
 
-#Enter the radius of a circle:
+#Enter the radius of a circle :
 #5
 #Area of the circle: 78.50
 #Circumference of the circle: 31.40
